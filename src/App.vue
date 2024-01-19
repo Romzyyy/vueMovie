c675855e3ee2591b2606a741bffd58da92862ccf
romzyyy <fijay0117@gmail.com>
2024-01-19 22:43:38 +0700

    feature: fetch api from tmdb

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
-<script setup></script>
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            movie: {},
+        }
+    },
+    mounted() {
+       axios.get('https://api.themoviedb.org/3/discover/movie?api_key=1faf3d5b91fec6bf481a8927461e1814').then((dataMovie) => {
+            this.movie = dataMovie.data.results
+        })
+    },
+}
+</script>
 
 <template>
-    <h1>hello world</h1>
+    <ul>
+      <li v-for="item in movie">{{ item.title }}</li>
+    </ul>
 </template>
---
+++ src/apiKey.js
@@ -0,0 +1,6 @@
+export default {
+    omdb: {
+        key: process.env.API_KEY,
+        endPoint: 'https://api.themoviedb.org/3',
+    },
+}
